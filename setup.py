5deb87bec7a37cbc74a9800bfd7363f9dffb45f2
Pau Freixes <pau.freixes@skyscanner.net>
2020-05-25 21:01:22 +0200

    Add support for manylinux1
    Pypi forces to upload packages that are compatible with all linux, we
    have modified the release workflow following the instruction for
    compiling a wheel that would need to be compatible for any linux.
    
    Because of that the release workflow has changed a bit, and Mac and
    Linux follow two different paths for compiling the wheel.
    
    Once the wheel is generated we test it by installing it into a new
    environment.

--- setup.py
+++ setup.py
@@ -28,7 +28,6 @@ dev_requires = [
     "pytest==5.4.1",
     "pytest-mock==3.1.0",
     "pytest-asyncio==0.11.0",
-    "uvloop==0.14.0",
     "asynctest==0.13.0",
     "pytest-cov==2.8.1",
     "black==19.10b0",
