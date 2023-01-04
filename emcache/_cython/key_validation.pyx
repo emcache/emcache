ec81526dd6582c8505089341731833013a2287b5
Justin Richert <justin.richert@life.church>
2023-01-04 10:46:05 -0600

    Adjust key validation algorithm to support characters supported by memcached protocol

--- emcache/_cython/key_validation.pyx
+++ emcache/_cython/key_validation.pyx
@@ -1,8 +1,10 @@
+from libc.stdio cimport printf
+
 cdef int MAX_KEY_LENGTH = 250
 
 def is_key_valid(bytes key) -> bool:
     """Validates if the key is an acceptable Memcache key.
-    
+
     From Memcache documentation, the key must not include control characters
     or whitespace, and must not take more than 250 characters.
     """
@@ -11,8 +13,11 @@ def is_key_valid(bytes key) -> bool:
     if len(key) > MAX_KEY_LENGTH:
         return False
 
-    for c in key:
-        if c < 33 or c > 126:
+    ustring = key.decode("UTF-8")
+
+    for uchar in ustring:
+        c = ord(uchar)
+        if c <= b' ' or c == 0x7f:
             return False
 
     return True
