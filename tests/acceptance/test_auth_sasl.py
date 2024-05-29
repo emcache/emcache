1440418ff2e62bcf8cc392c34e9ef6c39cfc6492
ArtemIsmagilov <artem.ismagilov.2000@mail.ru>
2024-05-29 12:26:03 +0400

    extend

--- tests/acceptance/test_auth_sasl.py
+++ tests/acceptance/test_auth_sasl.py
@@ -2,7 +2,7 @@ import sys
 
 import pytest
 
-from emcache import create_client
+from emcache import create_client, StorageCommandError
 
 pytestmark = pytest.mark.asyncio
 
@@ -10,7 +10,14 @@ pytestmark = pytest.mark.asyncio
 class TestAuth:
     @pytest.mark.skipif(sys.platform == "darwin", reason="This server is not built with SASL support.")
     async def test_auth(self, memcached_address_4, auth_username_password, key_generation):
+        key_and_value = next(key_generation)
+
         client = await create_client([memcached_address_4])
 
+        with pytest.raises(StorageCommandError):
+            await client.get(key_and_value)
+
         assert await client.auth(memcached_address_4, *auth_username_password) is None
+        assert await client.get(key_and_value) is None
+
         await client.close()
