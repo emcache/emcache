a70d24e4b94cd2b6516295af9a9e45f76aeb34ee
ArtemIsmagilov <artem.ismagilov.2000@mail.ru>
2024-07-11 12:58:13 +0400

    implement shutdown memcached command

--- tests/unit/test_client.py
+++ tests/unit/test_client.py
@@ -774,6 +774,23 @@ class TestClient:
         with pytest.raises(CommandError):
             await client.verbosity(memcached_host_address, -1)
 
+    async def test_shutdown_client_closed(self, client, memcached_host_address):
+        await client.close()
+        with pytest.raises(RuntimeError):
+            await client.shutdown(memcached_host_address)
+
+    async def test_shutdown_error_command(self, client, memcached_host_address):
+        # patch what is necesary for returnning an error string
+        connection = AsyncMock()
+        connection.shutdown_command = AsyncMock(return_value=ERROR)
+        connection_context = AsyncMock()
+        connection_context.__aenter__.return_value = connection
+        node = Mock()
+        node.connection.return_value = connection_context
+        client._cluster.node.return_value = node
+        with pytest.raises(CommandError):
+            await client.shutdown(memcached_host_address)
+
 
 async def test_create_client_default_values(event_loop, mocker):
     client_class = mocker.patch("emcache.client._Client")
