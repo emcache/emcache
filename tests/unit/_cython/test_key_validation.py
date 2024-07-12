5f8184d66bf6c4214946c744b79fa5fdae378779
ArtemIsmagilov <artem.ismagilov.2000@mail.ru>
2024-07-12 15:28:14 +0400

    currection little mistakes tests key is valid

--- tests/unit/_cython/test_key_validation.py
+++ tests/unit/_cython/test_key_validation.py
@@ -9,7 +9,7 @@ from emcache._cython import cyemcache
 class TestIsKeyValid:
     @pytest.mark.parametrize("key", [b"foo", b"bar", b"foobar", b"foobarfoo", b"123foobarfoo123", "ñ".encode("utf8")])
     def test_valid_keys(self, key):
-        assert not cyemcache.is_key_valid(key)
+        assert cyemcache.is_key_valid(key)
 
     @pytest.mark.parametrize(
         "key",
