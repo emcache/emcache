53c5479beeb8a40995cb63af16bfad76721909eb
ArtemIsmagilov <artem.ismagilov.2000@mail.ru>
2024-07-18 17:39:45 +0400

    change `|` on Union for python (<=3.10)

--- emcache/client.py
+++ emcache/client.py
@@ -941,7 +941,7 @@ class _Pipeline(Pipeline):
                 raise ValueError("Key has invalid charcters")
         return b"%b %a %b\r\n" % (command, exptime, b" ".join(keys))
 
-    async def execute(self) -> list[str | dict]:
+    async def execute(self) -> list[Union[str, dict]]:
         """Accumulate commands and push on memcached server."""
         if self.client._closed:
             raise RuntimeError("Emcache client is closed")
